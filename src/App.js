bff8abf86ccf75acb0e1a935844e4a6b0085dd69
1000peach <_970813@daum.net>
2020-03-14 00:57:19 +0900

    [add/delete] 프로젝트 이미지 추가 및 기타 삭제
    - README.md에 필요한 프로젝트 메인 이미지 추가
    - 불필요한 부분, 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-/* src/app.js */
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MainFrame from './login/MainFrame';
 import ServiceFrame from './service/ServiceFrame';
-//import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
-
-  componentDidMount() {
-  }
-
   render() {
     return (
       <div className='App'>
